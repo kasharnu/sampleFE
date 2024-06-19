386d2c075e001535f8fe49a6c9fe467efc997b8b
student <srk.hiremath@gmail.com>
2024-06-19 16:57:20 +0530

    today workout

---
+++ dynamic.js
@@ -0,0 +1,26 @@
+// var myname="Raja Rama Mohan Roy"
+// console.log(myname.length)
+// var newstr=myname.slice(5,11)
+// console.log(newstr)
+// var newstr1=myname.substr(2,2)
+// console.log(newstr1)
+  
+
+
+// var a=new Array();
+// a[0]=21
+// console.log(a[0])
+
+// var animals=["tiger","lion","elephant","deer"];
+// console.log(animals[0])
+// console.log(animals.sort())
+
+fun()
+function fun()
+{
+    var flowers=["lily","rose","sunflower","disney"]
+    document.write(flowers);
+}
+
+
+
