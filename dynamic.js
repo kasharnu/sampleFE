88c6a04e06193a714541dc585b6f7fd3ae133f9c
student <srk.hiremath@gmail.com>
2024-06-22 17:36:15 +0530

    4th commit

--- dynamic.js
+++ dynamic.js
@@ -15,12 +15,79 @@
 // console.log(animals[0])
 // console.log(animals.sort())
 
-fun()
-function fun()
-{
-    var flowers=["lily","rose","sunflower","disney"]
-    document.write(flowers);
-}
+// fun()
+// function fun()
+// {
+//     var flowers=["lily","rose","sunflower","disney"]
+//     document.write(flowers);
+// }
+// let age=84
+// const check=age>=18?"can vote":"not eligible"
+// document.write(check)
+// let k=BigInt("562")
+// console.log(typeof(k))
+
+// let sy=Symbol("hello")
+// console.log(typeof(sy))
+
+
+// let ys="kannada"
+// let jk="kannafa"
+// if(ys===jk)
+// {
+//     console.log("equal ")
+// }
+// else{
+//     console.log("no")
+// }
+
+// const pen={
+//     name: "raynolds",
+//     price:25,
+//     color: "red",
+//     store:"vidya sagar"
+// }
+
+// console.log(" i am kashinath","i purchased",pen.name,
+//     "for",pen.price,"rupees","which is",pen.color,"in color in",pen.store,"bookstall")
+
+
+//     console.log(` i am kashinath i purchased ${pen.name} pen  at ${pen.price} and that is ${pen.color} color i  bought this pen in ${pen.store}`);
+
+
+let name1="kiran"
+let name2="kiran"
+
+// // if(name1===name2)
+// // {
+// //     console.log("yes");
+// // }
+// // else{
+// //     console.log("no");
+// // }
+//  name1===name2 ? alert("yes"): console.log("no");
+// alert("hello welcome");
+
+
+
+
+
+
+// console.log(` i am kashi i purchased ${pen.name} at ${pen.price} 
+//     which is ${pen.color} in color in ${pen.store}bookstall`);
+
+let nums=[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+console.log(nums.splice(2,2,3,4))
+
+
+// random=10;
+// let guess_no=prompt("Enter a random number 1 to 10:")
+// while(guess_no!=random){
+//     console.log("Try again...");
+//     guess_no=prompt("Enter a random number");
+// }
+// if(guess_no==random){
+//     console.log("Congrats correct number!");}
 
 
 
